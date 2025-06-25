08d03791eedbba6e4b840f34ad2f3bfe11711125
pranavking <pranavkarmacharya@gmail.com>
2025-06-25 21:33:55 +0545

    adding back end

--- src/app/page.js
+++ src/app/page.js
@@ -9,11 +9,6 @@ import GiftCardSlider from "./component/Carousel/giftCardSlider";
 export default function Home() {
   return (
     <div>
-      <div className="flex text-white gap-10 pl-[138px] py-2">
-        {/* <h3>Discovery</h3>
-        <h3>Browse</h3>
-        <h3>News</h3> */}
-      </div>
       <div>
       </div>
       <div>
